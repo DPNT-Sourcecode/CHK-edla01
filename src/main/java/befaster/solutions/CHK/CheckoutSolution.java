a4f08b0d29dc9cd5830496ecce9373cfce973c1e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 15:52:03 +0100

    Thu Jun 14 15:52:03 BST 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,39 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.HashMap;
 
 public class CheckoutSolution {
+	/**
+	 * Calculate value of basket
+	 * @param skus a String containing the SKUs of all the products in the basket
+	 * @return Integer value
+	 */
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    	int						totalValue 		= 0;
+    	String 					aSku			= 0;
+    	Integer					aSkuCount		= null;
+    	HashMap<String,Integer>	skusInBasket	= new HashMap<String,Integer>();
+    	
+    	// Decode
+    	if ( skus != null && skus.length() > 0)
+    	{
+    		for (int index=0;index <skus.length(); index++)
+    		{
+    			aSku = new String(skus.substring(index, index+1));
+    			
+    			if (skusInBasket.containsKey(aSku) )
+    			{
+    				aSkuCount = skusInBasket.get(aSku);
+    				skusInBasket.put(aSku, Integer.valueOf(aSkuCount.intValue()+1) );
+    			}
+    			else
+    			{
+    				skusInBasket.put(aSku, Integer.valueOf(1) );
+    			}
+    		}
+    	}
+    	
+    	
+    	return Integer.valueOf(totalValue);
     }
 }
