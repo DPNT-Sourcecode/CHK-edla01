e75fe0db3db48500bdbd635ae95954d0914ed843
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 14:49:27 +0100

    Thu Jun 14 14:49:27 BST 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,11 +1,16 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
 
 public class SumSolution {
 
+	/**
+	 *  Sum two integers
+	 *  @param x = a positive integer between 0-100
+ 		@param y = a positive integer between 0-100
+ 		@return = an Integer representing the sum of the two numbers
+	 */
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+    	return x+y;
     }
 
 }
