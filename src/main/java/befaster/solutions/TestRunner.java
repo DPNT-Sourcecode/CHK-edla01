d2693b8b3bc26778679dec6ef0d0fa5aff39d413
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 14:58:27 +0100

    Thu Jun 14 14:58:27 BST 2018

--- src/main/java/befaster/solutions/TestRunner.java
+++ src/main/java/befaster/solutions/TestRunner.java
@@ -1,10 +1,21 @@
 package befaster.solutions;
 
+import org.junit.runner.junitcore;
+import org.junit.runner.notification.Failure;
+
+import befaster.solutions.SUM.TestSumSolution;
+
 public class TestRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Result  result = JUnitCore.runClasses(TestSumSolution.class);
+		
+		for (Failure failure: result.getFailures())
+		{
+			System.out.println(failure.toString());
+		}
+		System.out.println(result.wasSuccessful());
 	}
 
 }
