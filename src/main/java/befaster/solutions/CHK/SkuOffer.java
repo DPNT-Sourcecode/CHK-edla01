79651c8b2f0373bb3d25ced081fcab73cd4515c2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 16:06:28 +0100

    Thu Jun 14 16:06:28 BST 2018

--- src/main/java/befaster/solutions/CHK/SkuOffer.java
+++ src/main/java/befaster/solutions/CHK/SkuOffer.java
@@ -1,5 +1,28 @@
 package befaster.solutions.CHK;
 
-public class SkuOffer {
-
+public class SkuOffer 
+{
+	private	int	offerQuantity;
+	private int offerPrice;
+	
+	public SkuOffer(int quantity, int price)
+	{
+		offerQuantity = quantity;
+		offerPrice = price;
+	}
+	
+	public int getPrice()
+	{
+		return offerPrice;
+	}
+	
+	public int getQuantity()
+	{
+		return offerQuantity;
+	}
+	
+	public boolean isEligible(int aQuantity)
+	{
+		return aQuantity >  offerQuantity;
+	}
 }
