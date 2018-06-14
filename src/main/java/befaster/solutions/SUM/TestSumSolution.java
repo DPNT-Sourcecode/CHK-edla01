a806eb17b9b3ddea28a5aa6b84fc0bce2703ff3c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 14:46:28 +0100

    Thu Jun 14 14:46:28 BST 2018

---
+++ src/main/java/befaster/solutions/SUM/TestSumSolution.java
@@ -0,0 +1,14 @@
+package befaster.solutions.SUM;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSumSolution {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
