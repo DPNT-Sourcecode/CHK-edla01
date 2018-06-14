e143c0dbdfb359cce81bdeba7b22ca57ece554ac
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 15:15:41 +0100

    Thu Jun 14 15:15:41 BST 2018

--- src/main/java/befaster/solutions/HLO/TestHello.java
+++ src/main/java/befaster/solutions/HLO/TestHello.java
@@ -1,8 +1,9 @@
 package befaster.solutions.HLO;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 class TestHello {
 
