3b89859880343118b1cc9d5a7f56ccee7428935f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-14 16:15:28 +0100

    Thu Jun 14 16:15:28 BST 2018

--- src/main/java/befaster/solutions/CHK/SKU.java
+++ src/main/java/befaster/solutions/CHK/SKU.java
@@ -1,5 +1,52 @@
 package befaster.solutions.CHK;
 
-public class SKU {
+import java.util.*;
 
+public class SKU 
+{
+	private	String			id;
+	private int				price;
+	private List<SkuOffer>	offers;
+	
+	public SKU(String theId, int thePrice)
+	{
+		id 			= theId;
+		price 		= thePrice;
+		offers 		= new ArrayList<SkuOffer>();
+		
+	}
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	/**
+	 * Get (best) price
+	 * This is dependent on quantity
+	 * @param quantity
+	 * @return
+	 */
+	public int getBestPrice(int forQuantity)
+	{
+		
+		int	bestPrice 	= forQuantity * price;
+		int	offerPrice	= 0;
+		
+		if ( !offers.isEmpty() )
+		{
+			for ( SkuOffer anOffer: offers)
+			{
+				if ( anOffer.isEligible(forQuantity) )
+				{
+					offerPrice = anOffer.getPrice() * forQuantity;
+					if ( offerPrice < bestPrice )
+					{
+						bestPrice = offerPrice;
+					}
+				}
+			}
+		}
+		return bestPrice;
+	}
 }
